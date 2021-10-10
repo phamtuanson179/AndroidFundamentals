015f1cd5c094ab5865268e305504e4746902b10f
Phạm Tuấn Sơn <phamtuanson2000@gmail.com>
2021-10-10 23:47:00 +0700

    code done

--- 1-get-started/PartA_Your_first_interactive_UI/app/src/main/java/com/example/parta_your_first_interactive_ui/MainActivity.java
+++ 1-get-started/PartA_Your_first_interactive_UI/app/src/main/java/com/example/parta_your_first_interactive_ui/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.parta_your_first_interactive_ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
+    public void countUp(View view) {
+        ++mCount;
+        if(mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
+
     }
 }
