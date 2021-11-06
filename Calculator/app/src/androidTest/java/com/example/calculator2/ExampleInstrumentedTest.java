7bfed0d42a30adbaa6ba52e81c29c9e6b8ddbead
phamtuanson179 <62193655+phamtuanson179@users.noreply.github.com>
2021-11-06 09:27:34 +0700

    done calculator

--- Calculator/app/src/androidTest/java/com/example/calculator2/ExampleInstrumentedTest.java
+++ Calculator/app/src/androidTest/java/com/example/calculator2/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.calculator;
+package com.example.calculator2;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.calculator", appContext.getPackageName());
+        assertEquals("com.example.calculator2", appContext.getPackageName());
     }
 }
--- Calculator/app/src/test/java/com/example/calculator2/ExampleUnitTest.java
+++ Calculator/app/src/test/java/com/example/calculator2/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.calculator;
+package com.example.calculator2;
 
 import org.junit.Test;
 
